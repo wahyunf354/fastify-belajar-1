edb77b3d25e8ceb2edb573ce01d315d0042bf267
wahyunf354 <wahyunurfadillah313@gmail.com>
2020-11-22 20:12:50 +0700

    validate data

--- our-first-route.js
+++ our-first-route.js
@@ -1,7 +1,34 @@
-async function routes(fastify, options) {
+async function routes(fastify, options, done) {
+  
+  const opts = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          id: {type: 'number'},
+          key: {type: 'number'}
+        }
+      },
+      querystring: {
+        type: 'object',
+        properties: {
+          id: {type: 'number'}
+        }
+      }
+    }
+  }
+  
   fastify.get("/", async (request, reply) => {
     return { hello: "world" };
   });
+
+  fastify.post("/hello/:id",opts, async (req, rep) => {
+    const data = req.body.key;
+
+    return { hello: data };
+  });
+
+  done()
 }
 
 module.exports = routes;
